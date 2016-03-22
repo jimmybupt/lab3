f4d44835b60491fbbb395c73fccdb74fffd36963
Your Name <you@example.com>
2016-03-22 00:53:57 -0400

    added options

--- run.py
+++ run.py
@@ -1,1 +1,18 @@
+print 'CSE 5243 Prediction Analysis by Kun Liu & Zhe Dong'
 
+#program options
+from optparse import OptionParser
+import options
+parser = OptionParser()
+options.initialize_parser(parser)
+(options, args) = parser.parse_args()
+
+#load data file
+
+#identify training set / test set
+
+#train model
+
+#run model
+
+#report model
