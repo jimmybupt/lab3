ac8653d3f5dc816dc7236184fb4b2d42fd5c165b
Your Name <you@example.com>
2016-03-22 23:32:58 -0400

    intermediate commit

--- options.py
+++ options.py
@@ -1,28 +1,11 @@
 from optparse import OptionParser
 
-def test():
-	print "test"
-
 def initialize_parser(parser):
 	parser.add_option("-f", "--file", dest="in_file",
 		 help="the input vector",metavar="FILE",default="vector1.txt")
 	parser.add_option("-o", "--output", dest="out_file",
 		  help="the output matrix file", metavar="FILE")
-	parser.add_option("-m", "--metric", dest="metric",
-		  metavar="[Euclidean|Manhattan]",
-		 default="euclidean", help="Type of similarity")
-	parser.add_option("-a", "--algorithm", dest="algorithm",
-		  metavar="[DBSCAN|Hierarchical]",
-		  default="DBSCAN")
-	parser.add_option("-e", "--eps", dest="epsilon", metavar="<Epsilon>",
-		  type="float", default=0.5)
-	parser.add_option("-M", "--min-sample", dest="min_sample", metavar="<Min samples>",
-		  type="int", default=5) 
-	parser.add_option("-t", "--test", dest="small_data",
-		  action="store_true", default=False)
-	parser.add_option("-k", dest="cluster", metavar="<Cluster>", type="int",
-		  default=2)
-	parser.add_option("-l", dest="linkage", metavar="[Ward|Average|Complete]",
-		  default="ward")
-	
+	parser.add_option("-m", "--model", dest="model",
+		  metavar="[KNN|DecisionTree|Bayesian]",
+		 default="DecisionTree", help="Type of classifier")
 	return parser
--- run.py
+++ run.py
@@ -9,7 +9,58 @@ options.initialize_parser(parser)
 
 #load data file
 
+vector_file = open(options.in_file, 'r')
+label_file = open('label.txt', 'r')
+
+import ast
+import sys
+from scipy.sparse import *
+info = open("info.txt",'r');
+rdim = int(info.readline())
+cdim = int(info.readline())
+
+cross_fold = 5
+block_size = rdim / 5
+S = []	#vectors
+L = []	#labels
+
+for i = 1 to cross_fold
+	size = block_size
+	if i == cross_fold:
+		size = rdim - ((cross_fold-1)*block_size)
+	M = lil_matrix((size, cdim))
+	V = [None] * size;
+	S.append(M)
+	L.append(V)
+
+import time
+start_time = time.time()
+
+print "Reading vectors... ",
+sys.stdout.flush()
+i = 0
+for line in vector_file:
+	data = ast.literal_eval(line);
+	for D in data:
+		S[i, int(D[0])] = float(D[1])
+	i=i+1
+	if i==rdim:
+		break
+label = []
+i = 0
+for line in label_file:
+	label.append(ast.literal_eval(line))
+	if i==rdim:
+		break
+
+print "done"
+S = S.tocsr()
+
+data_process_time= time.time()  - start_time
+
 #identify training set / test set
+
+
 
 #train model
 
